101f24b92064fa3af8df6c723f6ea63e80fdb801
Alexey Toklovich <alexey@toklovich.com>
2015-02-06 11:57:40 +0600

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,103 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            development: {
+                options: {
+                    paths: ["css"]
+                },
+                files: {"build/style.css": "less/style.less"}
+            },
+            production: {
+                options: {
+                    paths: ["css"],
+                    cleancss: true
+                },
+                files: {"build/style.css": "less/style.less"}
+            }
+        },
+
+        cssmin: {
+            combine: {
+                files: {
+                    'build/style.min.css': ['build/style.css']
+                }
+            }
+        },
+
+        concat: {
+            dist: {
+                src: [
+                    'bower_components/jquery/dist/jquery.js',
+                    'bower_components/bootstrap/dist/bootstrap.js',
+                    'bower_components/jquery.inputmask/dist/jquery.inputmask.bundle.js',
+                    'bower_components/jquery-validate/dist/jquery.validate.js',
+                    'bower_components/fotorama/fotorama.js',
+                    'bower_components/jquery.countdown/dist/jquery.countdown.js',
+                    'bower_components/jquery-animatenumber/jquery.animateNumber.js',
+                    'bower_components/spin.js/spin.js',
+                    'bower_components/spin.js/jquery.spin.js',
+                    'bower_components/Stickyfill/dist/stickyfill.js',
+                    'bower_components/nanoscroller/bin/javascripts/jquery.nanoscroller.js',
+                    'js/script.js'
+                ],
+                dest: 'build/script.js'
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'build/script.js',
+                dest: 'build/script.min.js'
+            }
+        },
+
+        sprite:{
+            less: {
+                src: 'images/sprites/*.png',
+                dest: 'build/spritesheet.png',
+                destCss: 'build/sprites-vars.less'
+            },
+            css: {
+                src: 'images/sprites/*.png',
+                dest: 'build/spritesheet.png',
+                destCss: 'build/sprites.less',
+                cssFormat: 'css',
+                cssOpts: {
+                    cssSelector: function (item) {
+                        if (item.name.indexOf('-hover') !== -1) {
+                            return '.bm-' + item.name.replace('-hover', ':hover');
+                        } else if (item.name.indexOf('-active') !== -1) {
+                            return '.bm-' + item.name.replace('-active', ':active');
+                        }
+                        return '.bm-' + item.name;
+                    }
+                }
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['js/**/*.js', 'images/sprites/*', 'less/**/*.less'],
+                tasks: ['sprite', 'less', 'cssmin', 'concat', 'uglify'],
+                options: {
+                    livereload: true,
+                    spawn: false
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-spritesmith');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sprite', 'less', 'cssmin', 'concat', 'uglify', 'watch']);
+
+};
---
+++ js/script.js
@@ -0,0 +1,165 @@
+function countDownInit(element) {
+    // Countdown timer. Example: <div data-countdown-timer data-countdown-finaldate="2016/01/01"></div>
+    element.each(function() {
+        var $this = $(this), finalDate = $(this).data('countdown-finaldate');
+        $this.countdown(finalDate, function(event) {
+            $this.html(event.strftime('%H:%M:%S'));
+        });
+    });
+}
+
+function animateNumberInit(element) {
+    element.each(function() {
+        var number = $(this).data('animate-number');
+        var speed = $(this).data('animate-number-speed') || 1000;
+        $(this).animateNumber({number: number}, speed);
+    });
+}
+
+function initBuyButtons(element) {
+    element.each(function(){
+        var button = $(this);
+        var quantityInput = button.siblings('input[name=quantity]');
+        var quantity = quantityInput.val();
+        var quantityBlock = '<div class="bm-quantity-changer">' +
+            '<span class="bm-quantity-changer-minus" data-quantity-change="minus"></span>' +
+            '<input type="text" pattern="[0-9.]+" class="bm-quantity-changer-input" data-quantity-input value="' + quantity + '">' +
+            '<span class="bm-quantity-changer-plus" data-quantity-change="plus"></span>' +
+            '</div>';
+
+        if (quantity > 0) {
+            button.html('');
+            button.append(quantityBlock);
+            button.addClass('is-active');
+        } else {
+            $(this).on('click', function() {
+                if (!button.attr('disabled')) {
+                    ++quantity;
+                    quantityInput.val(quantity);
+                    initBuyButtons($(this));
+                }
+
+                $(this).unbind('click');
+
+                return false;
+            });
+        }
+    });
+}
+
+function initContentModal() {
+    $('.main-content').append('<div class="main-content-modal"><div class="main-content-modal-inner"></div></div>');
+    $('.main-content-modal').height($(window).height() - 80);
+    $('.main-content-modal').css('top', $(window).scrollTop());
+    $('.main-content-modal').append('<a class="main-content-modal-close">');
+
+    $('.main-content-modal-close').on('click', function(){
+        $('.main-content-modal').fadeOut();
+    });
+}
+
+$(window).on('scroll resize', function() {
+    $('.main-content-modal').height($(window).height() - 80);
+    $('.main-content-modal').css('top', function(){
+        return $(window).scrollTop()
+    });
+});
+
+$(document).on('click', '[data-quantity-input]', function() {
+    if (!$(this).hasClass('active')) {
+        $(this).addClass('active');
+        $(this).parent().addClass('is-input-active');
+        $(this).select();
+
+        $(this).on('blur', function(){
+            $(this).removeClass('active');
+            $(this).parent().removeClass('is-input-active');
+        });
+    }
+});
+
+$(document).on('click', '[data-load-product-card]', function() {
+    $('.main-content-modal').fadeIn();
+    $('.main-content-modal').spin('large');
+
+    $.ajax({
+        url: "product-1.html",
+        cache: false
+    }).done(function(data){
+        $(data).find('[data-buy-button]');
+        $('.main-content-modal').spin(false);
+        $('.main-content-modal-inner').html(data);
+    });
+
+    return false;
+});
+
+$(document).on('click', '[data-quantity-change]', function() {
+    var operator = $(this).data('quantity-change');
+    var quantityInput = $(this).siblings('[data-quantity-input]');
+    var currentQuantity = +quantityInput.val();
+
+    if (operator == 'plus') {
+        quantityInput.val(currentQuantity+1);
+    } else if (operator == 'minus') {
+        quantityInput.val(currentQuantity-1);
+    }
+});
+
+$(document).ready(function(){
+    countDownInit( $('[data-countdowntimer]') );
+    animateNumberInit( $('[data-animate-number]') );
+    initBuyButtons($('[data-buy-button]'));
+    initContentModal();
+
+    $('.js-sticky').Stickyfill();
+    $('.js-fotorama').fotorama();
+
+    $(".js-nano").nanoScroller({
+        paneClass: 'js-nano-pane',
+        sliderClass: 'js-nano-slider',
+        contentClass: 'js-nano-content'
+    });
+
+    $(".js-validate").each(function(){
+        $(this).validate({
+            errorPlacement: function(error, element) {},
+            highlight: function(element) {
+                $(element).closest('.form-group').addClass('has-error');
+            },
+            unhighlight: function(element) {
+                $(element).closest('.form-group').removeClass('has-error');
+            }
+        });
+    });
+
+    $('[data-masked-input]').each(function(){
+        var mask = $(this).data('masked-input');
+        $(this).inputmask(mask);
+    });
+
+    $('[data-tab]').click(function() {
+        var element = $(this).attr('href');
+        var group = $(this).data('tab');
+
+        $('[data-tab="' + group + '"]').removeClass('active');
+        $(this).addClass('active');
+        $(group).removeClass('active');
+        $(element).addClass('active');
+
+        console.log($(element));
+        if ($(element).hasClass('js-nano')) {
+            $(element).nanoScroller({
+                paneClass: 'js-nano-pane',
+                sliderClass: 'js-nano-slider',
+                contentClass: 'js-nano-content'
+            });
+        }
+
+        return false;
+    });
+});
+
+$('.js-fotorama').on('fotorama:show', function (e, fotorama) {
+    countDownInit( $(fotorama.activeFrame.html).find('[data-countdown-fotorama]') );
+});
